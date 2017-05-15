d6b038a89cf331b26155e85989e90d9b6777ecf8
dheerajvarma <dheerajvarma.g@gmail.com>
2017-05-15 14:38:41 +0530

    edited

--- D6-04Dheeraj/Source Code/adding_logo_with_no_background.py
+++ D6-04Dheeraj/Source Code/adding_logo_with_no_background.py
@@ -1,18 +1,30 @@
 import cv2
 import numpy as np
-img2=cv2.imread ("images.jpg")
-img1=cv2.imread("new quantum.PNG")
-rows,cols,channels=img2.shape
-roi=img1[0:rows,0:cols]
-img2g=cv2.cvtColor(img2,cv2.COLOR_BGR2GRAY)
-ret,mask=cv2.threshold(img2g, 220,255,cv2.THRESH_BINARY_INV)
-#cv2.imshow('mask',mask)
-mask_inv=cv2.bitwise_not(mask)
-img_bg=cv2.bitwise_and(roi,roi,mask=mask_inv)
-img_fg=cv2.bitwise_and(img2,img2,mask=mask)
-dst=img_bg+img_fg
-img1[0:rows,0:cols]=dst
-cv2.imshow('res',img1)
-cv2.imwrite('result.jpg',img1)
+
+img2 = cv2.imread("images.jpg")
+img1 = cv2.imread("new quantum.PNG")
+
+
+rows, cols, channels = img2.shape  # Reading image details
+roi = img1[0:rows, 0:cols]
+
+
+img2g = cv2.cvtColor(img2, cv2.COLOR_BGR2GRAY)  # converting to grayscale
+# defining mask , makes our logo black and background white
+ret, mask = cv2.threshold(img2g, 220, 255, cv2.THRESH_BINARY_INV)
+
+# cv2.imshow('mask',mask)
+
+mask_inv = cv2.bitwise_not(mask)  # defining non_masked area
+# adds our inv_mask to region of image of main image
+img_bg = cv2.bitwise_and(roi, roi, mask=mask_inv)
+img_fg = cv2.bitwise_and(img2, img2, mask=mask)  # our logo after adding mask
+dst = img_bg+img_fg  # we get our logo with our original image background
+# swaps the region of image original image to logo with same backgound
+img1[0:rows, 0:cols] = dst
+
+
+cv2.imshow('res', img1)
+cv2.imwrite('result.jpg', img1)
 cv2.waitKey(0)
 cv2.destroyAllWindows()
